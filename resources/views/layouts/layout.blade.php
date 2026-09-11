f04a146ac08b5d945d3ccfd78e7a20fcb4de414d
HZLTECH1 <hossein90acc@gmail.com>
2026-09-12 03:23:16 +0330

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,10 @@
     <title>@yield('title')</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+<header class="bg-white p-2">
+    <a href="{{ route('index') }}">حساب کاربری</a>
+    <a href="{{ route('messages_index') }}">صفحه اصلی</a>
+</header>
 <body class="p-4 md:p-8 bg-gray-50 min-h-screen flex flex-col items-center">
     @yield('content')
 </body>
